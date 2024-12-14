e01a30d967eb36ee50a6c4766740f2ed639e62e6
LAPTOP-L4QLQTHA\Hy <ngxuanhy1405@gmail.com>
2024-12-14 11:09:21 +0700

    update ass v2

--- MCU_ASS_1/Core/Src/control_traffic.c
+++ MCU_ASS_1/Core/Src/control_traffic.c
@@ -40,27 +40,39 @@ void displayTraffic() {
 	if (status_RED_A == 1) {
 		HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, SET);
 		HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, SET);
+		HAL_GPIO_WritePin(D5_GPIO_Port, D5_Pin, SET);
+		HAL_GPIO_WritePin(D6_GPIO_Port, D6_Pin, SET);
 	}
 	if (status_YELLOW_A == 1) {
 		HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, RESET);
 		HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, SET);
+		HAL_GPIO_WritePin(D5_GPIO_Port, D5_Pin, RESET);
+		HAL_GPIO_WritePin(D6_GPIO_Port, D6_Pin, SET);
 	}
 	if (status_GREEN_A == 1) {
 		HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, SET);
 		HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, RESET);
+		HAL_GPIO_WritePin(D5_GPIO_Port, D5_Pin, SET);
+		HAL_GPIO_WritePin(D6_GPIO_Port, D6_Pin, RESET);
 	}
 	//group b
 	if (status_RED_B == 1) {
 		HAL_GPIO_WritePin(D7_GPIO_Port, D7_Pin, SET);
 		HAL_GPIO_WritePin(D8_GPIO_Port, D8_Pin, SET);
+		HAL_GPIO_WritePin(D9_GPIO_Port, D9_Pin, SET);
+		HAL_GPIO_WritePin(D10_GPIO_Port, D10_Pin, SET);
 	}
 	if (status_YELLOW_B == 1) {
 		HAL_GPIO_WritePin(D7_GPIO_Port, D7_Pin, RESET);
 		HAL_GPIO_WritePin(D8_GPIO_Port, D8_Pin, SET);
+		HAL_GPIO_WritePin(D9_GPIO_Port, D9_Pin, RESET);
+		HAL_GPIO_WritePin(D10_GPIO_Port, D10_Pin, SET);
 	}
 	if (status_GREEN_B == 1) {
 		HAL_GPIO_WritePin(D7_GPIO_Port, D7_Pin, SET);
 		HAL_GPIO_WritePin(D8_GPIO_Port, D8_Pin, RESET);
+		HAL_GPIO_WritePin(D9_GPIO_Port, D9_Pin, SET);
+		HAL_GPIO_WritePin(D10_GPIO_Port, D10_Pin, RESET);
 	}
 }
 
